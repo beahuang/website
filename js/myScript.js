80fc069396798b98cb283c48e62a62bf80b8e6f4
Alfred Ababio <aababio75@gmail.com>
2014-04-30 21:55:08 -0400

    Moved smooth scrolling into the js file

--- js/myScript.js
+++ js/myScript.js
@@ -24,4 +24,17 @@ $(document).ready(function(){
 				$('#bigimg').html("<img src='images/bigmaze.jpg'>");
 				$('#desc').html('This was a lazer-cut model of a 3D maze');
 			});
+
+			$('a[href*=#]:not([href=#])').click(function() {
+				if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+					var target = $(this.hash);
+					target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+					if (target.length) {
+						$('html,body').animate({
+							scrollTop: target.offset().top
+						}, 1000);
+						return false;
+					}
+				}
+			});
 });
