985bd85f51a2e339fb9cabf6ca349c0ef8708cf7
Beatrice <huang.be@husky.neu.edu>
2014-04-30 20:04:48 -0400

    Initial commit with all the stuffies

---
+++ js/myScript.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+			//click function
+			$("#img1").click(function() {
+				$('#bigimg').html("<img src='images/bigbull.jpg'>");
+				$('#desc').html('This was a 3D Carving of a Bull from a Dry Foam Block');
+			});
+			$("#img2").click(function() {
+				$('#bigimg').html("<img src='images/bigcolor.jpg'>");
+				$('#desc').html('This a photoshop color collage that combined paintings and nature');
+			});
+			$("#img3").click(function() {
+				$('#bigimg').html("<img src='images/bigpaint.jpg'>");
+				$('#desc').html('This was the painted version of my photoshop color collage using acrylic paint');
+			})
+			$("#img4").click(function() {
+				$('#bigimg').html("<img src='images/bigdrag.jpg'>");
+				$('#desc').html('This was an art piece that used grids and was drawn using sharpie');
+			});
+			$("#img5").click(function() {
+				$('#bigimg').html("<img src='images/bigfish.jpg'>");
+				$('#desc').html('This was an art installation inside a building made with tape and saran wrap');
+			});
+			$("#img6").click(function() {
+				$('#bigimg').html("<img src='images/bigmaze.jpg'>");
+				$('#desc').html('This was a lazer-cut model of a 3D maze');
+			});
+});
