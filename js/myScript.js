6b3e76c7e4afd93c6b045c7aca540047d553d7a2
Beatrice <huang.be@husky.neu.edu>
2014-05-01 01:48:22 -0400

    add in extra images and fix style issues

--- js/myScript.js
+++ js/myScript.js
@@ -1,27 +1,27 @@
 $(document).ready(function(){
 			//click function
 			$("#img1").click(function() {
-				$('#bigimg').html("<img src='images/bigbull.jpg'>");
+				$('#bigimg').html("<img src='images/bigbull.jpg' class='img-responsive'>");
 				$('#desc').html('This was a 3D Carving of a Bull from a Dry Foam Block');
 			});
 			$("#img2").click(function() {
-				$('#bigimg').html("<img src='images/bigcolor.jpg'>");
+				$('#bigimg').html("<img src='images/bigcolor.jpg' class='img-responsive'>");
 				$('#desc').html('This a photoshop color collage that combined paintings and nature');
 			});
 			$("#img3").click(function() {
-				$('#bigimg').html("<img src='images/bigpaint.jpg'>");
+				$('#bigimg').html("<img src='images/bigpaint.jpg' class='img-responsive'>");
 				$('#desc').html('This was the painted version of my photoshop color collage using acrylic paint');
 			})
 			$("#img4").click(function() {
-				$('#bigimg').html("<img src='images/bigdrag.jpg'>");
+				$('#bigimg').html("<img src='images/bigdrag.jpg' class='img-responsive'>");
 				$('#desc').html('This was an art piece that used grids and was drawn using sharpie');
 			});
 			$("#img5").click(function() {
-				$('#bigimg').html("<img src='images/bigfish.jpg'>");
+				$('#bigimg').html("<img src='images/bigfish.jpg' class='img-responsive'>");
 				$('#desc').html('This was an art installation inside a building made with tape and saran wrap');
 			});
 			$("#img6").click(function() {
-				$('#bigimg').html("<img src='images/bigmaze.jpg'>");
+				$('#bigimg').html("<img src='images/bigmaze.jpg' class='img-responsive'>");
 				$('#desc').html('This was a lazer-cut model of a 3D maze');
 			});
 
